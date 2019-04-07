985d3d8677c2f859306122c629afc52e015e179d
bcid <bcid@uci.edu>
2019-04-06 23:01:46 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Brenda Cid on 4/6/19.
+//  Copyright © 2019 Brenda Cid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
